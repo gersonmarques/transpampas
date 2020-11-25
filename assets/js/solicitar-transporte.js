04f6549f83f8d20c58d3200072049c36727d0b9c
Bruno dos Reis <brunoreisdev@outlook.com>
2020-11-24 23:14:45 -0300

    :construction: Creating request transport page

--- assets/js/solicitar-transporte.js
+++ assets/js/solicitar-transporte.js
@@ -2,8 +2,8 @@ const PESSOA_FISICA = 'pessoa_fisica'
 const PESSOA_JURIDICA = 'pessoa_juridica'
 
 $(document).ready(function() {
-
-    $(document).on('change', '.form-check-input', function() {
+    masks();
+    $(document).on('change', '.tipo_dados_pessoais', function() {
 
         if (PESSOA_FISICA === $(this).val() && $(this).is(':checked')) {
             $('#pessoa_fisica_wrapper').show();
@@ -15,4 +15,47 @@ $(document).ready(function() {
             $('#pessoa_fisica_wrapper').hide();
         }
     });
+
+    $(document).on('change', '.tipo_origem', function() {
+        const ORIGEM_LEVAR = "levar_veiculo"
+        const ORIGEM_BUSCAR = "buscar_veiculo"
+
+        if (ORIGEM_LEVAR === $(this).val() && $(this).is(':checked')) {
+            $('#origem-levar').show();
+            $('#origem-buscar').hide();
+        }
+
+        if (ORIGEM_BUSCAR === $(this).val() && $(this).is(':checked')) {
+            $('#origem-levar').hide();
+            $('#origem-buscar').show();
+        }
+    })
+
+    $(document).on('change', '.tipo_destino', function() {
+        const DESTINO_LEVAR = "levem_veiculo"
+        const DESTINO_RETIRAR = "retirar_veiculo"
+
+        if (DESTINO_LEVAR === $(this).val() && $(this).is(':checked')) {
+            $('#destino-levar').show();
+            $('#destino-retirar').hide();
+        }
+
+        if (DESTINO_RETIRAR === $(this).val() && $(this).is(':checked')) {
+            $('#destino-levar').hide();
+            $('#destino-retirar').show();
+        }
+    })
+
 })
+
+function masks() {
+    $('.cpf').mask('000.000.000-00');
+    $('.whatsapp').mask('(00) 00000-0000');
+    $('.telefone-fixo').mask('(00) 00000-0000');
+
+    $('.cnpj').mask('00.000.000/0000-00');
+    $('.dtnasc-responsavel').mask('00/00/0000');
+
+
+    $('.cep').mask('00000-000');
+}
--- wp-content/themes/hello-elementor/teste.php
+++ wp-content/themes/hello-elementor/teste.php
@@ -8,6 +8,7 @@ get_header();
 <link rel="stylesheet" type="text/css" href="<?= get_site_url()."/assets/vendor/bootstrap/css/bootstrap.min.css" ?>" />
 <link rel="stylesheet" type="text/css" href="<?= get_site_url()."/assets/css/solicitar-transporte.css" ?>" />
 <script src="<?= get_site_url()."/assets/vendor/jquery/jquery-3.5.1.min.js"?>"></script>
+<script src="<?= get_site_url()."/assets/vendor/jquery/jquery.mask.js"?>"></script>
 <script src="<?= get_site_url()."/assets/vendor/bootstrap/js/bootstrap.min.js"?>"></script>
 <script src="<?= get_site_url()."/assets/js/solicitar-transporte.js"?>"></script>
 
@@ -18,11 +19,11 @@ get_header();
     </div>    
     <div class="container">
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="pessoa_fisica" value="pessoa_fisica" checked>
+            <input class="form-check-input tipo_dados_pessoais" type="radio" name="inlineRadioOptions" id="pessoa_fisica" value="pessoa_fisica" checked>
             <label class="form-check-label" for="pessoa_fisica">Pessoa física</label>
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="pessoa_juridica" value="pessoa_juridica">
+            <input class="form-check-input tipo_dados_pessoais" type="radio" name="inlineRadioOptions" id="pessoa_juridica" value="pessoa_juridica">
             <label class="form-check-label" for="pessoa_juridica">Pessoa jurídica</label>
         </div>
         <div class='container-fields col-md-8'>
@@ -33,11 +34,8 @@ get_header();
                 <?= pessoa_juridica(); ?>
             </div>
 
-            <?= origem(); ?>
-            <?= destino(); ?>
+            <?= documentos(); ?>    
         </div>
-
-      
         
 
         <div>
@@ -49,19 +47,23 @@ get_header();
 <?php 
 get_footer();
 
-
+/**
+ * 
+ *          <?= origem(); ?>
+ *          <?= destino(); ?>
+ */
 
 function pessoa_fisica()
 { 
     return "
         <div class='dados-pessoais section-data'>
             <p class='title-section'>Dados Pessoais</p>
-            <input type='text' class='input-field' maxLength='11' name='cpf' placeholder='CPF' />
+            <input type='text' class='input-field cpf' maxLength='11' name='cpf' placeholder='CPF' />
             <input type='text' class='input-field' name='nome' placeholder='Nome' />
-            <input type='text' class='input-field' name='e-mail' placeholder='E-mail' />
+            <input type='email' class='input-field' name='e-mail' placeholder='E-mail' />
             <div class='group-fields'>
-                <input type='tel' class='input-field' name='whatsapp' placeholder='Whatsapp' />
-                <input type='tel' class='input-field' name='telefone-fixo' placeholder='Telefone Fixo' />
+                <input type='tel' class='input-field whatsapp' name='whatsapp' placeholder='Whatsapp' />
+                <input type='tel' class='input-field telefone-fixo ' name='telefone-fixo' placeholder='Telefone Fixo' />
             </div>
         </div>
         <div class='dados-veiculos section-data'>
@@ -90,7 +92,7 @@ function pessoa_fisica()
             <input type='text' class='input-field' name='razao-social' placeholder='Razão Social' />
             <div class='group-fields'>
                 <input type='text' class='input-field' name='nome-responsavel' placeholder='Nome do responsável' />
-                <input type='text' class='input-field' name='dtnasc-responsavel' placeholder='Data Nasc. responsável' />
+                <input type='text' class='input-field dtnasc-responsavel' name='dtnasc-responsavel' placeholder='Data Nasc. responsável' />
             </div>
         </div>
         <div class='dados-veiculos section-data'>
@@ -114,20 +116,33 @@ function origem() {
         <div class='origem section-data'>
             <p class='title-section'>Origem</p>
             <div class="form-check ">
-                <input class="form-check-input position-static" type="radio" name="origem" id="levar_veiculo" value="levar_veiculo" checked>
+                <input class="form-check-input position-static tipo_origem" type="radio" name="origem" id="levar_veiculo" value="levar_veiculo" checked>
                 <label class="form-check-label" for="levar_veiculo">Vou <strong>levar</strong> o automóvel até o pátio da transportadora</label>
             </div>
             <div class="form-check">
-                <input class="form-check-input position-static" type="radio" name="origem" id="buscar_veiculo" value="buscar_veiculo">
+                <input class="form-check-input position-static tipo_origem" type="radio" name="origem" id="buscar_veiculo" value="buscar_veiculo">
                 <label class="form-check-label" for="buscar_veiculo">Quero que <strong>busquem</strong> o automóvel no meu endereço</label>
             </div>
 
-            <div>
+            <div id="origem-levar">
                 <div class='group-fields'>
                     <input type='text' class='input-field' name='estado' placeholder='Estado' />
                     <input type='text' class='input-field' name='cidade' placeholder='Cidade' />
                 </div>
                 <input type='text' class='input-field' name='endereco' placeholder='Endereço' />
+            </div>
+
+            <div id="origem-buscar">
+                <div class='group-fields'>
+                    <input type='tel' class='input-field' name='cep' placeholder='CEP' />
+                    <input type='text' class='input-field' name='estado' placeholder='Estado' />
+                    <input type='text' class='input-field' name='cidade' placeholder='Cidade' />
+                    <input type='text' class='input-field' name='bairro' placeholder='Bairro' />
+                </div>
+                <div class='group-fields-1-3'>
+                    <input type='text' class='input-field field-major' name='endereco' placeholder='Digite o endereço' />
+                    <input type='tel' class='input-field' name='numero' placeholder='Número' />
+                </div>
             </div>
         </div>
 <?php           
@@ -138,22 +153,54 @@ function destino() {
         <div class='destino section-data'>
             <p class='title-section'>Destino</p>
             <div class="form-check ">
-                <input class="form-check-input position-static" type="radio" name="destino" id="retirar_veiculo" value="retirar_veiculo" checked>
+                <input class="form-check-input position-static tipo_destino" type="radio" name="destino" id="retirar_veiculo" value="retirar_veiculo" checked>
                 <label class="form-check-label" for="retirar_veiculo">Vou <strong>retirar</strong> o automóvel no pátio da transportadora</label>
             </div>
             <div class="form-check">
-                <input class="form-check-input position-static" type="radio" name="destino" id="levem_veiculo" value="levem_veiculo">
+                <input class="form-check-input position-static tipo_destino" type="radio" name="destino" id="levem_veiculo" value="levem_veiculo">
                 <label class="form-check-label" for="levem_veiculo">Quero que <strong>levem</strong> o automóvel no meu endereço</label>
             </div>
 
-            <div>
+            <div  id="destino-retirar">
                 <div class='group-fields'>
                     <input type='text' class='input-field' name='estado' placeholder='Estado' />
                     <input type='text' class='input-field' name='cidade' placeholder='Cidade' />
                 </div>
                 <input type='text' class='input-field' name='endereco' placeholder='Endereço' />
             </div>
+
+            <div id="destino-levar">
+                <div class='group-fields'>
+                    <input type='tel' class='input-field' name='cep' placeholder='CEP' />
+                    <input type='text' class='input-field' name='estado' placeholder='Estado' />
+                    <input type='text' class='input-field' name='cidade' placeholder='Cidade' />
+                    <input type='text' class='input-field' name='bairro' placeholder='Bairro' />
+                </div>
+                <div class='group-fields-1-3'>
+                    <input type='text' class='input-field field-major' name='endereco' placeholder='Digite o endereço' />
+                    <input type='tel' class='input-field' name='numero' placeholder='Número' />
+                </div>
+            </div>
         </div>
 <?php           
+}
+
+function documentos()
+{ ?>
+  
+    <div class='documentos section-data'>
+        <p class='title-section'>Documentação</p>
+
+        <div class='group-fields'>
+            <input class="rg_cnh" type="file" id="rg_cnh" name="rg_cnh" style="display:none">
+            <label class="rg_cnh_label label_input">Cópia/Foto legível de RG OU CNH (.pdf .jpg .png)</label>
+            
+            <input class="crlv" type="file" id="crlv" name="crlv" style="display:none">
+            <label class="crlv_label label_input">Cópia/Foto legível de CRLV (.pdf .jpg .png)</label>
+        </div>
+    </div>
+
+
+<?php
 }
 ?>
